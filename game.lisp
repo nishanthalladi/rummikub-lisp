61125d8c208ce35d010852df8a88354e2e3c6ca6
nishanth <nishanthalladi01@gmail.com>
2021-04-04 03:17:26 -0400

    write take-sequence

--- game.lisp
+++ game.lisp
@@ -1,7 +1,8 @@
 (ql:quickload "alexandria")
 
-(defmacro firsts (a) `(loop for i in ,a collect (first i)))
-(defmacro seconds (a) `(loop for i in ,a collect (second i)))
+(defun firsts (a) (loop for i in a collect (first i)))
+(defun seconds (a) (loop for i in a collect (second i)))
+(defun list-equal (lst) (every #'equal lst (rest lst)))
 
 ; returns a hand through values as a list of size size and the new deck 
 ; ex: (hand '(1 2 3 4 5) 2) => (1 2) (3 4 5)
@@ -12,37 +13,47 @@
             cards-copy))
 
 ; returns a deck of 106 cards in the format (COLOR NUMBER) ex: (RED 12). Jokers are valued -1
-(defun setup_deck ()
+(defun setup-deck ()
     (setf colors (list 'red 'blue 'orange 'black))
     (setf numbers (loop for i from 1 to 13 collect i))
     (setf cards (alexandria:map-product 'list colors numbers))
     (alexandria:shuffle (append cards cards '(black -1) '(red -1))))
 
+; returns the modified hand given the sequence 
+; ex: (take-sequence '(1 2 3 4 5) '(3 4 5)) => (1 2)
+(defun take-sequence (hand seq)
+    (when (not (and (check-sequence seq) (has-seq hand seq))) return hand)
+    (loop for card in hand unless (member card seq :test #'equal) collect card))
+
+(defun has-seq (hand seq)
+    (equal seq (loop for card in hand when (member card seq) collect card)))
+
 ; checks whether a sequence of cards is valid
-; ex: (check_sequence '((RED 3) (RED 1) (RED 2))) => T
-; ex: (check_sequence '((RED 3) (RED 3) (RED 1) (RED 2))) => nil
-; ex: (check_sequence '((RED 5) (BLUE 5) (BLACK 5))) => T
-; ex: (check_sequence '((RED 5) (BLUE 5) (BLUE 5) (BLACK 5))) => nil
-(defun check_sequence (seqo)
+; ex: (check-sequence '((RED 3) (RED 1) (RED 2))) => T
+; ex: (check-sequence '((RED 3) (RED 3) (RED 1) (RED 2))) => nil
+; ex: (check-sequence '((RED 5) (BLUE 5) (BLACK 5))) => T
+; ex: (check-sequence '((RED 5) (BLUE 5) (BLUE 5) (BLACK 5))) => nil
+(defun check-sequence (seqo)
     (cond ((< (length seqo) 3) nil)
-          ((every #'equal (firsts seqo) (rest (firsts seqo))) (seq_check_nums (sort (seconds seqo) #'<)))
-          ((every #'equal (seconds seqo) (rest (seconds seqo))) (seq_check_color (firsts seqo)))
+          ((list-equal (firsts seqo)) (seq-check-nums (sort (seconds seqo) #'<)))
+          ((list-equal (seconds seqo)) (seq-check-color (firsts seqo)))
           (t nil)))
 
-(defun seq_check_color (seq)
+(defun seq-check-color (seq)
     (when (or (< 4 (length seq)) (> 3 (length seq))) (return nil))
     (equal (remove-duplicates seq) seq))
 
 ; checks whether a sequence of numbers is valid
-; ex: (seq_check_nums '(0 1 2 3 4 5)) => T
-; ex: (seq_check_nums '(0 1 2 3 6 5)) => nil
-(defun seq_check_nums (seq)
-    (let ((seqc (flatten_joker seq)))
+; ex: (seq-check-nums '(0 1 2 3 4 5)) => T
+; ex: (seq-check-nums '(0 1 2 3 6 5)) => nil
+(defun seq-check-nums (seq)
+    (let ((seqc (flatten-joker seq)))
         (equal seqc (loop repeat (length seq) for i from (first seqc) collect i))))
 
 ; change joker into number that fits in the sequence
-; ex: (flatten_joker '(0 1 2 -1 4 5)) => (0 1 2 3 4 5)
-(defun flatten_joker (seq)
+; ex: (flatten-joker '(0 1 2 -1 4 5)) => (0 1 2 3 4 5)
+(defun flatten-joker (seqo)
+    (setf seq (copy-list seqo))
     (when (= (first seq) -1) (setf (first seq) (- (second seq) 1)))
     (loop for i to (- (length seq) 1) 
         if (= (nth i seq) -1) do 
